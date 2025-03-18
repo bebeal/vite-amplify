87d623e83ec2409ec9f6c77aae44870345ad2010
Benjamin Noah Beal <bnoahbeal@gmail.com>
2025-03-18 00:07:32 -0700

    Fix static file serving

--- server.ts
+++ server.ts
@@ -39,7 +39,7 @@ export const createServer = async (root = process.cwd(), env = process.env.NODE_
     app.use(vite.middlewares);
   } else {
     // add static file serving for production builds
-    app.use((await import('compression')).default);
+    app.use((await import('compression')).default());
     app.use(
       (await import('serve-static')).default(path.resolve(__dirname, '../client'), {
         index: false,
