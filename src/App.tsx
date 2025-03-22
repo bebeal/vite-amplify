6ae1b0cf285880dbbb89d695894569a25b211e7a
Benjamin Noah Beal <bnoahbeal@gmail.com>
2025-03-22 00:08:50 -0700

    Cleanup DemoPage UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { DemoPage } from './components/DemoPage';
 import { ThemeToggle } from './components/ThemeToggle/ThemeToggle';
 
 const App = () => {
   return (
     <div className='w-full h-screen flex flex-col transition-all duration-300 overflow-auto'>
       <ThemeToggle />
+      <DemoPage repo="bebeal/vite-amplify" />
     </div>
   );
 };
